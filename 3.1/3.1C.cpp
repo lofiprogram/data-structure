6499db369fcd72177dad9ec8b87a18f62a04e4ed
lofiprogram <47585732+lofiprogram@users.noreply.github.com>
2020-12-24 23:12:10 +0800

    Update 3.1C.cpp

--- 3.1/3.1C.cpp
+++ 3.1/3.1C.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main() {
 	int a, b;
-  //若直接赋值给int数组a，b，例如值24，此时a[0]=24,而不是a[0]=2。
+	//若直接赋值给int数组a，b，例如值24，此时a[0]=24,而不是a[0]=2。
 	while (scanf("%d%d", &a, &b) != EOF) {
 		int c[10] = {}, d[10] = {};
 		int sum = 0;
@@ -14,7 +14,7 @@ int main() {
 			c[i] = a % 10;
 			a /= 10;
 		}
-    //十进制求每位上的数。
+		//十进制求每位上的数。
 		for (int i = 0; b != 0; i++) {
 			d[i] = b % 10;
 			b /= 10;
