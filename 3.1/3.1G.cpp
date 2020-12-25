6641a208f63552ab5162d7ca21053760eae948c0
lofiprogram <47585732+lofiprogram@users.noreply.github.com>
2020-12-25 15:39:59 +0800

    Create 3.1G.cpp

---
+++ 3.1/3.1G.cpp
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	int n;
+	while(scanf("%d",&n)!=EOF){
+		vector<int>str(n);
+		int sum1 = 0, m1 = 0, sum2 = 0, m2 = 0;
+		int n1 = 0, n2 = 0, x = 1, k = 0;
+		double sum3 = 0;
+		for (int i = 0; i < n; i++) {
+			scanf("%d", &str[i]);
+
+			if (str[i] % 5 == 0 && str[i] % 2 == 0) {
+				m1++;
+				sum1 += str[i];
+			}
+
+			if (str[i] % 5 == 1) {
+				m2++;
+				sum2 += x * str[i];
+				x *= -1;
+			}
+
+			if (str[i] % 5 == 2)
+				n1++;
+
+			if (str[i] % 5 == 3) {
+				n2++;
+				sum3 += str[i];
+			}
+
+			if (str[i] % 5 == 4 && str[i] > k)
+				k = str[i];
+		}
+		if (m1 == 0)
+			printf("N ");
+		else
+			printf("%d ", sum1);
+		if (m2 == 0)
+			printf("N ");
+		else
+			printf("%d ", sum2);
+		if (n1 == 0)
+			printf("N ");
+		else
+			printf("%d ", n1);
+		if (n2 == 0)
+			printf("N ");
+		else
+			printf("%.1lf ", sum3/n2);
+		if (k == 0)
+			printf("N\n");
+		else
+			printf("%d\n", k);
+	}
+	return 0;
+}
+
