f9e2d4d24980034851e7537ef766a54a474823b9
lofiprogram <47585732+lofiprogram@users.noreply.github.com>
2020-12-25 16:34:32 +0800

    Update 3.1H.cpp

--- 3.1/3.1H.cpp
+++ 3.1/3.1H.cpp
@@ -9,6 +9,7 @@ int main() {
 	long long a, b;
 	int c, d;
 	while (scanf("%lld%d%lld%d", &a, &c, &b, &d) != EOF) {
+		//long long型数据的输入输出均为%lld。
 		long long sum1 = 0, sum2 = 0;
 		int t1 = c, t2 = d;
 		for (int i = 0; i < 10; i++) {
