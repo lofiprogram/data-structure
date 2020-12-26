4114e6660e7a0a6b576be3e704747ed45273977a
lofiprogram <47585732+lofiprogram@users.noreply.github.com>
2020-12-26 21:55:51 +0800

    Create 3.2E.cpp

---
+++ 3.2查找元素/3.2E.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<vector>
+#include<cstring>
+using namespace std;
+
+struct students {
+	int id;
+	char name[50];
+	char sex[20];
+	int age;
+}stu[20];
+
+int main() {
+	int n;
+	while (scanf("%d", &n) != EOF) {
+		while (n--) {
+			int m;
+			scanf("%d", &m);
+			for (int i = 0; i < m; i++) {
+				scanf("%d%s%s%d", &stu[i].id, stu[i].name, stu[i].sex, &stu[i].age);
+			}
+			int t;
+			scanf("%d", &t);
+			for (int i = 0; i < m; i++) {
+				if (t == stu[i].id)
+					printf("%d %s %s %d\n", stu[i].id, stu[i].name, stu[i].sex, stu[i].age);
+			}
+		}
+	}
+	return 0;
+}
+
+	
+
