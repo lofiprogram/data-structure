e785fdb0ab2d1100318f7efac6769b2bc670d9c4
lofiprogram <47585732+lofiprogram@users.noreply.github.com>
2020-12-26 19:00:17 +0800

    Update 3.2A.cpp

--- 3.2查找元素/3.2A.cpp
+++ 3.2查找元素/3.2A.cpp
@@ -7,6 +7,8 @@ int main() {
 	int n;
 	while (scanf("%d", &n) != EOF) {
 		if (n == 0)break;
+		//注意阅读题目条件当读到N=0时输入结束。
+		//出错情况：结果错误。
 		int str[1000] = {};
 		for (int i = 0; i < n; i++) {
 			scanf("%d", &str[i]);
