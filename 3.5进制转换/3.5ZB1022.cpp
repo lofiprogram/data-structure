024737d1a39a88f4b373d6f9375cd8928da0b14f
lofiprogram <47585732+lofiprogram@users.noreply.github.com>
2020-12-27 18:51:23 +0800

    Update 3.5ZB1022.cpp

--- 3.5进制转换/3.5ZB1022.cpp
+++ 3.5进制转换/3.5ZB1022.cpp
@@ -13,6 +13,17 @@ int main() {
 	if (sum = 0) {
 		printf("0");
 	}
+	
+	//有一个案例没通过即没考虑sum=0的情况。
+	//该种情况下使用do while语句就不用单独分析了。
+	
+	/*
+	do{
+		s[i++]=sum % D;
+		sum =sum / D;
+	}while(sum != 0);
+	*/
+	
 	for (; sum!=0; i++) {
 		s[i] = sum % D;
 		sum = sum / D;
