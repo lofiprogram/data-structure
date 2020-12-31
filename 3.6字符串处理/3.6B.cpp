1f2fce6f8d6b63d326dea30106af38f6d128288b
lofiprogram <47585732+lofiprogram@users.noreply.github.com>
2020-12-31 18:56:44 +0800

    Update 3.6B.cpp

--- 3.6字符串处理/3.6B.cpp
+++ 3.6字符串处理/3.6B.cpp
@@ -9,6 +9,7 @@ using namespace std;
 int main(){
 	char a[101];
 	while (gets_s(a)) {
+		//使用scanf无法读入' '，故使用gets。
 		int i = 0;
 		int len = strlen(a);
 		for (; i<len ; i++){
@@ -16,10 +17,12 @@ int main(){
 			if (a[0] >= 97 && a[0] <= 122) {
 				a[0] -= 32;
 			}
+			//首字母。
 
 			if (a[i] == ' ' && a[i + 1] >= 97 && a[i + 1] <= 122) {
 				a[i + 1] -= 32;
 			}
+			//不仅需要为' '，还需要其后为小写字母。
 
 			if (a[i] == '\t' && a[i + 1] >= 97 && a[i + 1] <= 122) {
 				a[i + 1] -= 32;
@@ -31,7 +34,8 @@ int main(){
 
 		}
 		puts(a);
-		printf("%s\n", a);
+		//printf("%s\n", a);
+		//puts函数会在字符串末尾自动添加\n，printf需要如上才有相同效果。
 	}
     return 0;
 }
