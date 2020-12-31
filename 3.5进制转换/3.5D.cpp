62725bff0d9344247edb03ca5a604ea1ba3c7e90
lofiprogram <47585732+lofiprogram@users.noreply.github.com>
2020-12-31 17:10:29 +0800

    Create 3.5D.cpp

---
+++ 3.5进制转换/3.5D.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<cstdio>
+#include<cstring>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	int n;
+	while (scanf("%d", &n) != EOF) {
+		int a[1000] = {};
+		int i = 0;
+		do {
+			a[i++] = n % 8;
+			n = n / 8;
+		} while (n != 0);
+		i = i - 1;
+		for (; i >= 0; i--) {
+			printf("%d", a[i]);
+		}
+		printf("\n");
+	}
+    return 0;
+}
+
+	
+
