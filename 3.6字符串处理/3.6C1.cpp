121d9b7746042e2bfd6dc164a0dbf874bbba622f
lofiprogram <47585732+lofiprogram@users.noreply.github.com>
2021-01-01 15:28:03 +0800

    Update 3.6C1.cpp

--- 3.6字符串处理/3.6C1.cpp
+++ 3.6字符串处理/3.6C1.cpp
@@ -16,6 +16,7 @@ int main(){
 				a[i] += 32;
 			}
 		}
+	        //转换为全小写进行比较，以实现不区分大小写的进行删除。
 
 		char b1[1000];
 		while (gets_s(b1)) {
@@ -41,9 +42,11 @@ int main(){
 					F++;
 				}
 			}
+			//找出每一个相同字符串的位置。
 
 			//for (int i = 0; i < F; i++)
 				//printf("%d ", Find[i]);
+			//测试时进行检验是否找出。
 
 			for (int i = 0; i < F; i++) {
 				int m = Find[i]-(len1*i);
@@ -52,6 +55,7 @@ int main(){
 					b1[j] = b1[j + len1];
 				}
 			}
+			//删除代码中含有字符串的部分。
 
 			for (int i = 0; i < len2; i++) {
 				if (b1[i] == ' ') {
@@ -60,6 +64,7 @@ int main(){
 				printf("%c", b1[i]);
 			}
 			printf("\n");
+			//删除多余的空格。（题目虽未要求，但样例输出中删除了。）
 
 
 		}
