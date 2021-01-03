c6c7865c02a05069005faeab044ce725ed61a433
lofiprogram <47585732+lofiprogram@users.noreply.github.com>
2021-01-03 20:31:59 +0800

    Create 6.1B2.cpp

---
+++ 6.1vector的常见用法详解/6.1B2.cpp
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+#include<iostream>
+#include<vector>
+#include <algorithm>
+using namespace std;
+
+const int N = 40010;
+const int K = 2510;
+
+vector<int>course[K];
+char name[N][5];
+
+bool cmp(int a, int b) {
+	return strcmp(name[a], name[b]) < 0;
+}
+
+
+int main() {
+	int n, k;
+	scanf("%d%d", &n, &k);
+
+	for (int i = 0; i < n; i++) {
+		int id, num;
+		scanf("%s %d", name[i], &num);
+		for (int j = 0; j < num; j++) {
+			scanf("%d", &id);
+			course[id].push_back(i);
+		}
+	}
+
+	for (int i = 1; i <= k; i++) {
+		printf("%d %d", i, course[i].size());
+		sort(course[i].begin(), course[i].end(), cmp);
+		for (int j = 0; j < course[i].size(); j++) {
+			int t = course[i][j];
+			printf("\n%s", name[t]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+	
+
