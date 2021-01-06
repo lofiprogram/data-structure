0ba7c5919581c96ae9c3254fda08ca2f95411d21
lofiprogram <47585732+lofiprogram@users.noreply.github.com>
2021-01-06 21:34:28 +0800

    Update 6.3 Z A1060 2.cpp

--- CHAP6 C++标准模板库（STL）/6.3 string的常见用法详解/6.3 Z A1060 2.cpp
+++ CHAP6 C++标准模板库（STL）/6.3 string的常见用法详解/6.3 Z A1060 2.cpp
@@ -17,36 +17,55 @@ string deal(string s,int& e) {
 	while (s.length() > 0 && s[0] == '0') {
 		s.erase(s.begin());
 	}
+	//该循环去除前导零
+	
+	if (s.length() == 0) {
+		e = 0;
+	}
+	//去除前导零后s的长度为0，该数即为0
 
 	if (s[0] == '.') {
+		//去除前导0后第一位数为"."，说明s是小于1的数
 		s.erase(s.begin());
+		//去掉小数点
 		while (s.length() > 0 && s[0] == '0') {
 			s.erase(s.begin());
+			//去掉小数点后非零位前所有的零
+			//即0.00027的情况
 			e--;
+			//e记录指数，每去掉一个0，减1
 		}
 	}
 	else {
+		//去掉前导零后第一位不是小数点的情况
 		while (k < s.length() && s[k] != '.') {
 			k++;
 			e++;
 		}
+		//统计小数点前一共多少位
+		//e记录指数，每多一位，指数加1
 		if (k < s.length()) {
+			//说明该情况下有小数点
 			s.erase(s.begin() + k);
+			//删除小数点
 		}
-	}
-	
-	if (s.length() == 0) {
-		e = 0;
 	}
 
 	int num = 0;
 	k = 0;
 	string res;
+	
 	while (num < n) {
-		if (k < s.length())res += s[k++];
-		else res += '0';
+		if (k < s.length()){
+		        res += s[k++];
+		}else{ 
+			res += '0';
+		     }
 		num++;
 	}
+	//精度小于n就进行循环
+	//s内有数字则加数字
+	//s内无数字则加0
 	return res;
 }
 
